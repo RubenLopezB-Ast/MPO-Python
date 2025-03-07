4563f78eb5e28397f178b4dedfd71d50f077ab10
RubenLopezB-Ast <rlb09@hotmail.es>
2025-03-07 05:54:02 +0100

    Primeros apuntes

---
+++ PythonProject/Clase 1.py
@@ -0,0 +1,45 @@
+print("Hola Mundo!")
+
+nombre = "Rubén L."         #String
+edad = 41                   #Entero o también llamado int
+altura = 1.81               #Float
+es_estudiante = True        #Booleano
+
+print(nombre,edad,altura,es_estudiante)
+
+# . Enteros (int)
+# Números sin decimal
+numero_entero= 43
+numero_negativo= -12
+
+print("Numero entero es:", numero_entero)
+print("Me imprimo un número negativo",numero_negativo)
+print("Quieto imprimir que tipo de dato tengo",type(numero_negativo))
+print("Queiro imprimir que tipo ee dato teigno", type(numero_entero))
+
+#. Decimales (float)
+#Los números flotantes son aquellos que llevan tílde.
+numero_decimal= 3.14
+temeperatura= -3.5
+print("numero decimal",numero_decimal)
+print("numero decimal coma",numero_decimal)
+print("Quiero imprimir que tipor de dato tengo AHORA", type(numero_decimal))
+# . Números complejos (complex)
+#Los números complejos tienen una parte real y una imaginaria (se usa "j").
+num_complejo=2 +3j
+print("Imprime el número complejo siguiente:",num_complejo)
+print("Imprime la parte real:",num_complejo.real,"|Parte imaginaria:",num_complejo.imag)
+print("Que tipo de dato soy:",type(num_complejo))
+suma = 10 + 5
+resta = 3-2
+multiplicacion = 2*3
+division=10/5
+division_entera=10//3
+modulo=10%3
+potencia=2**3
+print("Aquí tengo el valor de la suma",suma)
+print("Aqui el valor de la resta:", resta)
+print("Aquí el valor de la multiplicación", multiplicacion)
+print("Aquí el valor de la división", division)
+print("Aquí el valor de la dicisión entera", division_entera)
+print("Aquí el valor del módulo", modulo)
