e9f00645eb602210461e6b445e87c75f1efe510e
RubenLopezB-Ast <rlb09@hotmail.es>
2025-04-27 15:10:45 +0200

    Subida de ejercicios hasta el 17 incluido

--- PythonProject/Clase10Jordi/ejercici11Clase10.py
+++ PythonProject/Clase10Jordi/ejercici11Clase10.py
@@ -46,12 +46,52 @@ numero_e = int(input("Escribe un número entero positivo: "))
 for i in range(1,11,1):
     print(f"{i}x{numero_e}={numero_e*i}")
 
-"""Ejercicio 14¶
-Escribe un programa que pida al usuario un número entero positivo e imprima la suma de los números pares por un lado y 
-la suma de los números impares por otro. El programa debe imprimir ambos resultados."""
+"""Ejercicio 15¶
+Escribe un programa que pida al usuario un número entero positivo y calcules la suma de la potencia de 3 de cada número desde 1 hasta el número 
+introducido. El programa debe imprimir el resultado.
 
-numero_f = int(input("Escribe un número entero positivo(ej14): "))
+Para que se entienda mejor, si el usuario introduce 3, el programa debe calcular:"""
+
+numero_f = int(input("Escribe un número entero positivo(ej15): "))
 resultado_f= 0
 for i in range(1,numero_f+1,1):
     resultado_f += i ** 3
     print(f"La suma de la pontencia de 3 de los numeros desde 1 hasta {numero_f} es: {resultado_f}")
+
+
+"""Ejercicio 14¶
+Escribe un programa que pida al usuario un número entero positivo e imprima la suma de los números pares por un lado y la suma de los números impares 
+por otro. El programa debe imprimir ambos resultados."""
+
+numero_g = int(input("Escribe un número entero positivo: "))
+resultado_g1= 0
+resultado_g2 =0
+for i in range(1,numero_g+1,1):
+    if i%2 == 0:
+        resultado_g2 = resultado_g2 + i
+    else:
+        resultado_g1 = resultado_g1 + i
+
+print(f"La suma de números impares es: {resultado_g1}")
+print(f"Y la suma de números pares es: {resultado_g2}")
+
+"""Ejercicio 16¶
+Escribe un programa que pida al usuario un número entero positivo e imprima la lista de divisores de ese número. Un divisor de un número n es un número 
+entero que divide a n sin dejar residuo. El programa debe imprimir todos los divisores del número introducido."""
+
+numero_h = int(input("Escribe un número entero positivo (Ej:16): "))
+
+print(f"Los divisores de {numero_h} son: ", end=" ")
+for i in range(1, numero_h+1,1):
+    if numero_h % i == 0:
+        print(i, end=", ")
+print()
+
+"""Ejercicio 17¶
+Escribe un programa que reciba un número entero positivo y una letra. El programa debe imprimir la letra tantas veces como el número introducido."""
+
+numero_j = int(input("Escribe un número positivo (Ej:17): "))
+letra = str(input("Escribe una letra: "))
+
+for i in range (1, numero_j+1,1):
+    print(letra, end=", ")
